a16d2fa0051d1ded5405c852c842c81ca0bd7529
meshchem <meshchem@tcd.ie>
2026-04-13 21:44:30 +0100

    removing obsolete files and dataset, reorganising repositry, saving correct sampling file, removef all figures, updated some descriptions in notebook

--- Dataset_Sampling/convert_csv.py
+++ Dataset_Sampling/convert_csv.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # Load parquet
-df = pd.read_parquet('data_aug/tweets_keyword_engagement_2000.parquet')
+df = pd.read_parquet('sampled_data/tweets_keyword_engagement_2000.parquet')
+# df = pd.read_parquet('sampled_data/tweets_keyword_engagement_500.parquet')
+
 
 # # View first few rows
 # print(df.head())
@@ -19,9 +21,8 @@ df = pd.read_parquet('data_aug/tweets_keyword_engagement_2000.parquet')
 # print(df.iloc[0])
 
 # Save as CSV
-
-
-df.to_csv('../manual_annotations/sample_2000.csv', index=False)
+df.to_csv('../sampled_data/tweets_keyword_engagement_2000.csv', index=False)
+# df.to_csv('../sampled_data/tweets_keyword_engagement_500.csv', index=False)
 
 # Or save just a subset
 # df[['row_id', 'post_type', 'text', 'keyword_category',  'hashtags',  'like_count', 'date']].to_csv('../LLM_annotations/sampled_tweets_2000.csv', index=False)
